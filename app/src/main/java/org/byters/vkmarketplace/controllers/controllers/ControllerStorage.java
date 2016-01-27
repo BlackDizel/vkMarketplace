16fc9ba0cf77c414e083ac042f83cadb7568b20d
alexandr motorin <blackdizel@gmail.com>
2016-01-27 21:43:09 +0400

    add local storage controller

---
+++ app/src/main/java/org/byters/vkmarketplace/controllers/controllers/ControllerStorage.java
@@ -0,0 +1,85 @@
+package org.byters.vkmarketplace.controllers.controllers;
+
+import android.app.Activity;
+import android.content.Context;
+import android.util.Log;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+public class ControllerStorage {
+
+    public static final String ITEMS_INFO = "cache_items";
+    private static final String TAG = "controllerStorage";
+
+    public synchronized static void writeObjectToFile(Context context, Object object, String filename) {
+
+        ObjectOutputStream objectOut = null;
+        if (object == null)
+            context.deleteFile(filename);
+        else {
+            try {
+                FileOutputStream fileOut = context.openFileOutput(filename, Activity.MODE_PRIVATE);
+                objectOut = new ObjectOutputStream(fileOut);
+                objectOut.writeObject(object);
+                fileOut.getFD().sync();
+
+            } catch (IOException e) {
+                Log.e(TAG, Log.getStackTraceString(e));
+            } finally {
+                if (objectOut != null) {
+                    try {
+                        objectOut.close();
+                    } catch (IOException e) {
+                        Log.e(TAG, Log.getStackTraceString(e));
+                    }
+                }
+            }
+        }
+    }
+
+    public synchronized static Object readObjectFromFile(Context context, String filename) {
+
+        ObjectInputStream objectIn = null;
+        Object object = null;
+        boolean needRemove = false;
+        try {
+
+            FileInputStream fileIn = context.getApplicationContext().openFileInput(filename);
+            objectIn = new ObjectInputStream(fileIn);
+            object = objectIn.readObject();
+
+        } catch (FileNotFoundException e) {
+            Log.e(TAG, Log.getStackTraceString(e));
+        } catch (IOException | ClassNotFoundException e) {
+            Log.e(TAG, Log.getStackTraceString(e));
+            needRemove = true;
+        } finally {
+            if (objectIn != null) {
+                try {
+                    objectIn.close();
+                } catch (IOException e) {
+                    Log.e(TAG, Log.getStackTraceString(e));
+                }
+            }
+        }
+
+        if (needRemove) RemoveFile(context, filename);
+
+        return object;
+    }
+
+    public static synchronized void RemoveFile(Context ctx, String filename) {
+        try {
+            ctx.getApplicationContext().deleteFile(filename);
+        } catch (Exception e) {
+            Log.e(TAG, Log.getStackTraceString(e));
+        }
+    }
+
+
+}
