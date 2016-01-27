f5dc8722395a03f57d2b9c6254364fe337da90b4
alexandr motorin <blackdizel@gmail.com>
2016-01-27 21:42:56 +0400

    add main controller class

--- app/src/main/java/org/byters/vkmarketplace/controllers/ControllerMain.java
+++ app/src/main/java/org/byters/vkmarketplace/controllers/ControllerMain.java
@@ -2,5 +2,16 @@ package org.byters.vkmarketplace.controllers;
 
 import android.app.Application;
 
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
+
 public class ControllerMain extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        //todo set imageloader config
+        ImageLoader.getInstance().init(ImageLoaderConfiguration.createDefault(this));
+    }
 }
