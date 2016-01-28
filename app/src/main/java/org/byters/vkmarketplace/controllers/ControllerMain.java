ff4d7d65d5f6412f6def587415b929f287d54164
alexandr motorin <blackdizel@gmail.com>
2016-01-28 17:20:50 +0400

    add auth methods

--- app/src/main/java/org/byters/vkmarketplace/controllers/ControllerMain.java
+++ app/src/main/java/org/byters/vkmarketplace/controllers/ControllerMain.java
@@ -1,11 +1,16 @@
 package org.byters.vkmarketplace.controllers;
 
 import android.app.Application;
+import android.support.annotation.NonNull;
 
 import com.nostra13.universalimageloader.core.ImageLoader;
 import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
 
+import org.byters.vkmarketplace.controllers.controllers.ControllerAuth;
+
 public class ControllerMain extends Application {
+
+    private ControllerAuth controllerAuth;
 
     @Override
     public void onCreate() {
@@ -13,5 +18,14 @@ public class ControllerMain extends Application {
 
         //todo set imageloader config
         ImageLoader.getInstance().init(ImageLoaderConfiguration.createDefault(this));
+        controllerAuth = new ControllerAuth(this);
+    }
+
+    public boolean isAuth() {
+        return controllerAuth.isAuth();
+    }
+
+    public void setToken(@NonNull String key) {
+        controllerAuth.setToken(this, key);
     }
 }
