f487013e8d13c069047f0275ce4eebb9800ba932
alexandr motorin <blackdizel@gmail.com>
2016-01-29 17:06:11 +0400

    implement item id intent value

--- app/src/main/java/org/byters/vkmarketplace/ui/activities/ActivityItemInfo.java
+++ app/src/main/java/org/byters/vkmarketplace/ui/activities/ActivityItemInfo.java
@@ -1,14 +1,29 @@
 package org.byters.vkmarketplace.ui.activities;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 
 import org.byters.vkmarketplace.R;
 
 public class ActivityItemInfo extends ActivityBase {
 
+    public final static String INTENT_EXTRA_ITEM_ID = "item_id";
+    private final static int NO_VALUE = -1;
+
+    private int id;
+
+    public static void display(Context context, int id) {
+        Intent intent = new Intent(context, ActivityItemInfo.class);
+        intent.putExtra(ActivityItemInfo.INTENT_EXTRA_ITEM_ID, id);
+        context.startActivity(intent);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_item_info);
+
+        id = getIntent().getIntExtra(INTENT_EXTRA_ITEM_ID, NO_VALUE);
     }
 }
