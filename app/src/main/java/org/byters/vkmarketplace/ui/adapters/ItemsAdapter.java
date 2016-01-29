b5938a1364f0b7f16d25a3261eae800bce7be979
alexandr motorin <blackdizel@gmail.com>
2016-01-29 17:07:11 +0400

    implement item activity navigation

--- app/src/main/java/org/byters/vkmarketplace/ui/adapters/ItemsAdapter.java
+++ app/src/main/java/org/byters/vkmarketplace/ui/adapters/ItemsAdapter.java
@@ -1,5 +1,6 @@
 package org.byters.vkmarketplace.ui.adapters;
 
+import android.app.Activity;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -13,6 +14,7 @@ import com.nostra13.universalimageloader.core.ImageLoader;
 import org.byters.vkmarketplace.R;
 import org.byters.vkmarketplace.controllers.ControllerMain;
 import org.byters.vkmarketplace.model.dataclasses.MarketplaceItem;
+import org.byters.vkmarketplace.ui.activities.ActivityItemInfo;
 
 public class ItemsAdapter extends RecyclerView.Adapter<ItemsAdapter.ViewHolder> {
 
@@ -42,24 +44,33 @@ public class ItemsAdapter extends RecyclerView.Adapter<ItemsAdapter.ViewHolder> 
         notifyDataSetChanged();
     }
 
-    public class ViewHolder extends RecyclerView.ViewHolder {
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
         ImageView ivItem;
         TextView tvTitle, tvPrice;
+        private int id;
 
         public ViewHolder(View itemView) {
             super(itemView);
             tvTitle = (TextView) itemView.findViewById(R.id.tvTitle);
             tvPrice = (TextView) itemView.findViewById(R.id.tvPrice);
             ivItem = (ImageView) itemView.findViewById(R.id.ivItem);
+            itemView.setOnClickListener(this);
         }
 
         public void setData(int position) {
             MarketplaceItem item = controllerMain.getControllerItems().getModel().get(position);
-            if (item == null) return;
+            if (item == null) return; //maybe need to throw error here, hm?
             tvTitle.setText(item.getTitle());
             if (item.getPrice() != null)
                 tvPrice.setText(item.getPrice().getText());
             ImageLoader.getInstance().displayImage(item.getThumb_photo(), ivItem);
+            id = item.getId();
+        }
+
+        @Override
+        public void onClick(View v) {
+            if (v.getContext() instanceof Activity)
+                ActivityItemInfo.display(v.getContext(), id);
         }
     }
 }
