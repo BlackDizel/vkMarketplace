88d25838ae4c51aec00daad8c539ddf6d8452036
alexandr motorin <blackdizel@gmail.com>
2016-01-29 16:52:16 +0400

    add activity item info stub

---
+++ app/src/main/java/org/byters/vkmarketplace/ui/activities/ActivityItemInfo.java
@@ -0,0 +1,14 @@
+package org.byters.vkmarketplace.ui.activities;
+
+import android.os.Bundle;
+
+import org.byters.vkmarketplace.R;
+
+public class ActivityItemInfo extends ActivityBase {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_item_info);
+    }
+}
