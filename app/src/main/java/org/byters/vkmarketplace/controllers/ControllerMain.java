bc56e51c78f030d864f07314907fd13fda19bfb8
alexandr motorin <blackdizel@gmail.com>
2016-01-30 18:48:33 +0400

    add use controller item update cache

--- app/src/main/java/org/byters/vkmarketplace/controllers/ControllerMain.java
+++ app/src/main/java/org/byters/vkmarketplace/controllers/ControllerMain.java
@@ -7,15 +7,23 @@ import com.nostra13.universalimageloader.core.ImageLoader;
 import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
 
 import org.byters.vkmarketplace.controllers.controllers.ControllerAuth;
+import org.byters.vkmarketplace.controllers.controllers.ControllerItemInfo;
 import org.byters.vkmarketplace.controllers.controllers.ControllerItems;
+import org.byters.vkmarketplace.controllers.controllers.utils.OnItemUpdateListener;
+import org.byters.vkmarketplace.model.dataclasses.MarketplaceItem;
 
-public class ControllerMain extends Application {
+public class ControllerMain extends Application implements OnItemUpdateListener {
 
     private ControllerAuth controllerAuth;
     private ControllerItems controllerItems;
+    private ControllerItemInfo controllerItemInfo;
 
     public ControllerItems getControllerItems() {
         return controllerItems;
+    }
+
+    public ControllerItemInfo getControllerItemInfo() {
+        return controllerItemInfo;
     }
 
     @Override
@@ -26,6 +34,8 @@ public class ControllerMain extends Application {
         ImageLoader.getInstance().init(ImageLoaderConfiguration.createDefault(this));
         controllerAuth = new ControllerAuth(this);
         controllerItems = new ControllerItems(this, controllerAuth.getToken());
+        controllerItemInfo = new ControllerItemInfo();
+        controllerItemInfo.addItemInfoUpdatedListener(this);
     }
 
     public boolean isAuth() {
@@ -35,10 +45,25 @@ public class ControllerMain extends Application {
     public void setToken(@NonNull String key) {
         controllerAuth.setToken(this, key);
         controllerItems.updateData(this, key);
-    }
 
+        //todo update detailed item info
+    }
 
     public void updateMarketList() {
         controllerItems.updateData(this, controllerAuth.getToken());
+    }
+
+    public void updateDetailedItemInfo(int itemId) {
+        MarketplaceItem item = controllerItems.getModel().getItemById(itemId);
+        if (item != null && item.getPhotos() != null) {
+            controllerItemInfo.updateListeners(item);
+            return;
+        }
+        controllerItemInfo.getItemInfo(this, itemId, controllerAuth.getToken());
+    }
+
+    @Override
+    public void onItemLoaded(@NonNull MarketplaceItem item) {
+        controllerItems.getModel().setItem(this, item);
     }
 }
