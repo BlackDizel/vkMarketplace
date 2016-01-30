8f2256beb26c659ba449551e763fdecb4d591115
alexandr motorin <blackdizel@gmail.com>
2016-01-30 18:48:52 +0400

    implement updating item info

--- app/src/main/java/org/byters/vkmarketplace/ui/activities/ActivityItemInfo.java
+++ app/src/main/java/org/byters/vkmarketplace/ui/activities/ActivityItemInfo.java
@@ -3,7 +3,9 @@ package org.byters.vkmarketplace.ui.activities;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
 import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -13,11 +15,12 @@ import com.nostra13.universalimageloader.core.ImageLoader;
 import org.byters.vkmarketplace.R;
 import org.byters.vkmarketplace.controllers.ControllerMain;
 import org.byters.vkmarketplace.controllers.controllers.utils.ItemsUpdateListener;
+import org.byters.vkmarketplace.controllers.controllers.utils.OnItemUpdateListener;
 import org.byters.vkmarketplace.model.dataclasses.MarketplaceItem;
 
 import java.util.ArrayList;
 
-public class ActivityItemInfo extends ActivityBase implements ItemsUpdateListener {
+public class ActivityItemInfo extends ActivityBase implements ItemsUpdateListener, OnItemUpdateListener {
 
     public final static String INTENT_EXTRA_ITEM_ID = "item_id";
     private final static int NO_VALUE = -1;
@@ -44,11 +47,19 @@ public class ActivityItemInfo extends ActivityBase implements ItemsUpdateListene
     }
 
     private void setData() {
+        //todo add viewpager with photos
+
+
         if (id == NO_VALUE) return;
         MarketplaceItem item = ((ControllerMain) getApplicationContext()).getControllerItems().getModel().getItemById(id);
         if (item == null) return;
         ((TextView) findViewById(R.id.tvDebug)).setText(item.getTitle());
-        ImageLoader.getInstance().displayImage("http://lorempixel.com/400/300", (ImageView) findViewById(R.id.ivItem));
+
+
+        if (item.getPhotos() != null
+                && item.getPhotos().size() > 0 &&
+                !TextUtils.isEmpty(item.getPhotos().get(0).getSrc_big()))
+            ImageLoader.getInstance().displayImage(item.getPhotos().get(0).getSrc_big(), (ImageView) findViewById(R.id.ivItem));
     }
 
     //region listener
@@ -56,12 +67,16 @@ public class ActivityItemInfo extends ActivityBase implements ItemsUpdateListene
     protected void onResume() {
         super.onResume();
         ((ControllerMain) getApplicationContext()).getControllerItems().addListener(this);
+        ((ControllerMain) getApplicationContext()).getControllerItemInfo().addItemInfoUpdatedListener(this);
+
+        ((ControllerMain) getApplicationContext()).updateDetailedItemInfo(id);
     }
 
     @Override
     protected void onPause() {
         super.onPause();
         ((ControllerMain) getApplicationContext()).getControllerItems().removeListener(this);
+        ((ControllerMain) getApplicationContext()).getControllerItemInfo().removeItemUpdatedListener(this);
     }
     //endregion
 
@@ -75,6 +90,12 @@ public class ActivityItemInfo extends ActivityBase implements ItemsUpdateListene
     @Override
     public void onUpdated(ArrayList<MarketplaceItem> data) {
         if (data == null) return;
+        setData();
+    }
+
+    @Override
+    public void onItemLoaded(@NonNull MarketplaceItem item) {
+        if (item.getId() != id) return;
         setData();
     }
 }
