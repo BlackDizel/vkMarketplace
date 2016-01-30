15c207565d0bf548e5b4ca9b8c9c799ad0f65d7f
alexandr motorin <blackdizel@gmail.com>
2016-01-30 18:45:51 +0400

    add photos class

--- app/src/main/java/org/byters/vkmarketplace/model/dataclasses/MarketplaceItem.java
+++ app/src/main/java/org/byters/vkmarketplace/model/dataclasses/MarketplaceItem.java
@@ -1,6 +1,7 @@
 package org.byters.vkmarketplace.model.dataclasses;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 
 public class MarketplaceItem implements Serializable {
     private int id;
@@ -8,6 +9,11 @@ public class MarketplaceItem implements Serializable {
     private String description;
     private String thumb_photo;
     private Price price;
+    private ArrayList<MarketplaceItemPhoto> photos;
+
+    public ArrayList<MarketplaceItemPhoto> getPhotos() {
+        return photos;
+    }
 
     public String getTitle() {
         return title;
---
+++ app/src/main/java/org/byters/vkmarketplace/model/dataclasses/MarketplaceItemPhoto.java
@@ -0,0 +1,31 @@
+package org.byters.vkmarketplace.model.dataclasses;
+
+import java.io.Serializable;
+
+public class MarketplaceItemPhoto implements Serializable {
+    private String src_big;
+
+    public String getSrc_big() {
+        return src_big;
+    }
+
+/*
+{
+    "pid": 393555246,
+    "aid": -53,
+    "owner_id": -94800461,
+    "user_id": 100,
+    "src": "http:\/\/cs633119.vk.me\/v633119621\/2c9e\/jS9SOZJQdHM.jpg",
+    "src_big": "http:\/\/cs633119.vk.me\/v633119621\/2c9f\/kYWW3zt1VQg.jpg",
+    "src_small": "http:\/\/cs633119.vk.me\/v633119621\/2c9d\/ul3yuHYzMCA.jpg",
+    "src_xbig": "http:\/\/cs633119.vk.me\/v633119621\/2ca0\/FpoNbSZs_4Y.jpg",
+    "src_xxbig": "http:\/\/cs633119.vk.me\/v633119621\/2ca1\/cndUmcTR3Cc.jpg",
+    "src_xxxbig": "http:\/\/cs633119.vk.me\/v633119621\/2ca2\/ICs4RNhoTLw.jpg",
+    "width": 2560,
+    "height": 1920,
+    "text": "",
+    "created": 1449323142
+}
+*/
+
+}
