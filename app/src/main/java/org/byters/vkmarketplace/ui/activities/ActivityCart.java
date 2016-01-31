fd4d7972d81b91cd09eed02a7c2450a41dd6dae1
alexandr motorin <blackdizel@gmail.com>
2016-01-31 19:05:18 +0400

    add cart activity stub

---
+++ app/src/main/java/org/byters/vkmarketplace/ui/activities/ActivityCart.java
@@ -0,0 +1,39 @@
+package org.byters.vkmarketplace.ui.activities;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+
+import org.byters.vkmarketplace.R;
+
+public class ActivityCart extends ActivityBase {
+
+    public static void display(Context context) {
+        context.startActivity(new Intent(context, ActivityCart.class));
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cart);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        RecyclerView rvItems = (RecyclerView) findViewById(R.id.rvItems);
+        rvItems.setLayoutManager(new LinearLayoutManager(this));
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home)
+            onBackPressed();
+        return super.onOptionsItemSelected(item);
+    }
+}
