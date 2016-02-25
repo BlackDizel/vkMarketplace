d42bdaee11f19997e365b136e90569f33c29d1fa
alexandr motorin <blackdizel@gmail.com>
2016-02-25 19:55:01 +0400

    set requested orientation portrait

--- app/src/main/java/org/byters/vkmarketplace/ui/activities/ActivityBase.java
+++ app/src/main/java/org/byters/vkmarketplace/ui/activities/ActivityBase.java
@@ -1,6 +1,14 @@
 package org.byters.vkmarketplace.ui.activities;
 
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
 
 public class ActivityBase extends AppCompatActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+    }
 }
