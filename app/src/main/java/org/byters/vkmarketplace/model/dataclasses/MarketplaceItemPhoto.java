9d3295128f4722b529791d89ace0f7987af1f90d
alexandr motorin <blackdizel@gmail.com>
2016-03-05 20:21:40 +0400

    add marketplace item little photo

--- app/src/main/java/org/byters/vkmarketplace/model/dataclasses/MarketplaceItemPhoto.java
+++ app/src/main/java/org/byters/vkmarketplace/model/dataclasses/MarketplaceItemPhoto.java
@@ -4,6 +4,11 @@ import java.io.Serializable;
 
 public class MarketplaceItemPhoto implements Serializable {
     private String photo_1280;
+    private String photo_130;
+
+    public String getLittlePhoto() {
+        return photo_130;
+    }
 
     public String getSrc_big() {
         return photo_1280;
