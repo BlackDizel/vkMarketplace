e6158747f8ef23ec2a05a51b1fd9e9e605d90413
alexandr motorin <blackdizel@gmail.com>
2016-03-11 19:29:11 +0400

    login activity disclaimer added

--- app/src/main/java/org/byters/vkmarketplace/ui/activities/ActivityLogin.java
+++ app/src/main/java/org/byters/vkmarketplace/ui/activities/ActivityLogin.java
@@ -1,17 +1,22 @@
 package org.byters.vkmarketplace.ui.activities;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.net.UrlQuerySanitizer;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.Snackbar;
 import android.text.TextUtils;
+import android.view.View;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
 import org.byters.vkmarketplace.R;
 import org.byters.vkmarketplace.controllers.ControllerMain;
 
-public class ActivityLogin extends ActivityBase {
+public class ActivityLogin extends ActivityBase
+        implements View.OnClickListener {
 
     WebView webView;
 
@@ -19,6 +24,8 @@ public class ActivityLogin extends ActivityBase {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        findViewById(R.id.tvNavigate).setOnClickListener(this);
 
         webView = (WebView) findViewById(R.id.wvLogin);
         webView.setWebViewClient(new AuthClient());
@@ -33,6 +40,17 @@ public class ActivityLogin extends ActivityBase {
 
     private void loadUrl() {
         webView.loadUrl(getString(R.string.vk_auth_url));
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse(getString(R.string.market_address)));
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        } else {
+            Snackbar.make(findViewById(R.id.rootView), R.string.action_view_browser_error, Snackbar.LENGTH_SHORT).show();
+        }
     }
 
     private class AuthClient extends WebViewClient {
