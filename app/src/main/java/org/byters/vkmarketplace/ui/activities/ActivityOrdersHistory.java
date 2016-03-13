8f6d5bee0f8f55bc6307ddbf9c222fb9ef9701e3
alexandr motorin <blackdizel@gmail.com>
2016-03-13 20:27:22 +0400

    add orders history activity

--- app/src/main/java/org/byters/vkmarketplace/ui/activities/ActivityOrdersHistory.java
+++ app/src/main/java/org/byters/vkmarketplace/ui/activities/ActivityOrdersHistory.java
@@ -2,9 +2,16 @@ package org.byters.vkmarketplace.ui.activities;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Rect;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
 
 import org.byters.vkmarketplace.R;
+import org.byters.vkmarketplace.ui.adapters.OrdersHistoryAdapter;
 
 public class ActivityOrdersHistory extends ActivityBase {
 
@@ -17,7 +24,48 @@ public class ActivityOrdersHistory extends ActivityBase {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_orders_history);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        RecyclerView rvOrders = (RecyclerView) findViewById(R.id.rvOrders);
+        rvOrders.setLayoutManager(new LinearLayoutManager(this));
+        rvOrders.addItemDecoration(new ItemDecoration(this));
+        rvOrders.setAdapter(new OrdersHistoryAdapter(this));
     }
 
-    //todo implement
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    //region itemDecorator
+    private class ItemDecoration extends RecyclerView.ItemDecoration {
+
+        private int margin;
+
+        public ItemDecoration(Context context) {
+            margin = (int) context.getResources().getDimension(R.dimen.view_item_orders_list_margin);
+        }
+
+        @Override
+        public void getItemOffsets(Rect outRect, View view, RecyclerView parent, RecyclerView.State state) {
+            super.getItemOffsets(outRect, view, parent, state);
+
+            int position = parent.getChildLayoutPosition(view);
+            if (position == 0)
+                outRect.top = margin;
+            outRect.left = margin;
+            outRect.bottom = margin;
+            outRect.right = margin;
+        }
+    }
+    //endregion
 }
