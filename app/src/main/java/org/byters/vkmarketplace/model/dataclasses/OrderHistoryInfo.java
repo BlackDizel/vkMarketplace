10ce5f0a5a51d1bb81b6ce1b35e54f6db3ac7581
alexandr motorin <blackdizel@gmail.com>
2016-03-13 21:02:59 +0400

    implement Comparable, fix getText method first line string

--- app/src/main/java/org/byters/vkmarketplace/model/dataclasses/OrderHistoryInfo.java
+++ app/src/main/java/org/byters/vkmarketplace/model/dataclasses/OrderHistoryInfo.java
@@ -2,21 +2,26 @@ package org.byters.vkmarketplace.model.dataclasses;
 
 import android.content.Context;
 import android.support.annotation.NonNull;
+import android.text.TextUtils;
 
 import java.io.Serializable;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 
-public class OrderHistoryInfo implements Serializable {
+public class OrderHistoryInfo implements Serializable, Comparable<OrderHistoryInfo> {
     private long date;
     private int sum;
     private ArrayList<OrderItemHistoryInfo> items;
 
+    public long getRawDate() {
+        return date;
+    }
+
     public String getDate() {
         Date d = new Date();
         d.setTime(date);
-        return new SimpleDateFormat("dd MMMM yyyy").format(d);
+        return new SimpleDateFormat("dd MMMM yyyy HH:mm").format(d);
     }
 
     public int getSum() {
@@ -32,7 +37,10 @@ public class OrderHistoryInfo implements Serializable {
         if (items == null) return "";
         String result = "";
         for (OrderItemHistoryInfo item : items) {
-            result = String.format("%s\n%s", result, item.getText(context));
+            if (TextUtils.isEmpty(result))
+                result = item.getText(context);
+            else
+                result = String.format("%s\n%s", result, item.getText(context));
         }
         return result;
     }
@@ -45,4 +53,8 @@ public class OrderHistoryInfo implements Serializable {
         this.items = items;
     }
 
+    @Override
+    public int compareTo(OrderHistoryInfo another) {
+        return date < another.getRawDate() ? -1 : (date == another.getRawDate() ? 0 : 1);
+    }
 }
