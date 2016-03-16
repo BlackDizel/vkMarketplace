4e2598d0db48f29cd037bb53efa102d6f3d5bd2f
alexandr motorin <blackdizel@gmail.com>
2016-03-16 19:58:38 +0400

    add marketplace item method get album id

--- app/src/main/java/org/byters/vkmarketplace/model/dataclasses/MarketplaceItem.java
+++ app/src/main/java/org/byters/vkmarketplace/model/dataclasses/MarketplaceItem.java
@@ -9,6 +9,7 @@ import java.io.Serializable;
 import java.util.ArrayList;
 
 public class MarketplaceItem implements Serializable {
+    public static final int NO_VALUE = -1;
     private int id;
     private String title;
     private String description;
@@ -66,6 +67,12 @@ public class MarketplaceItem implements Serializable {
         if (position < 0 || albums_ids == null || position >= albums_ids.length)
             return null;
         return controllerMain.getControllerAlbums().getTitle(albums_ids[position]);
+    }
+
+    public int getAlbumId(int position) {
+        if (albums_ids == null || position < 0 || position >= albums_ids.length)
+            return NO_VALUE;
+        return albums_ids[position];
     }
 
     public class LikeInfo implements Serializable {
