549a13a1ce0edaf42d5649869a350e1360bfc010
katriniya <katriniya.a@gmail.com>
2016-11-24 19:23:09 +0300

    fix start snap helper

--- app/src/main/java/org/byters/vkmarketplace/ui/utils/StartSnapHelper.java
+++ app/src/main/java/org/byters/vkmarketplace/ui/utils/StartSnapHelper.java
@@ -1,5 +1,6 @@
 package org.byters.vkmarketplace.ui.utils;
 
+import android.graphics.Rect;
 import android.support.v7.widget.LinearSnapHelper;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
@@ -7,6 +8,8 @@ import android.view.View;
 public class StartSnapHelper extends LinearSnapHelper {
     @Override
     public int[] calculateDistanceToFinalSnap(RecyclerView.LayoutManager lm, View view) {
-        return new int[]{view.getLeft() - lm.getPaddingLeft(), 0};
+        Rect rect = new Rect();
+        lm.getDecoratedBoundsWithMargins(view, rect);
+        return new int[]{rect.left - lm.getPaddingLeft(), 0};
     }
 }
