7b3cd8854905f8c5fdf39df3798de5f4b68a5481
katriniya <katriniya.a@gmail.com>
2016-12-02 14:46:57 +0300

    set phone payment, vk payment and paypal payment disabled

--- app/src/main/java/org/byters/vkmarketplace/model/PaymentMethodEnum.java
+++ app/src/main/java/org/byters/vkmarketplace/model/PaymentMethodEnum.java
@@ -7,10 +7,11 @@ import org.byters.vkmarketplace.R;
 
 public enum PaymentMethodEnum {
 
-    TYPE_PHONE(R.drawable.ic_call_black_24dp, R.string.payment_method_phone),
+    //todo implement payment methods
+    //TYPE_PHONE(R.drawable.ic_call_black_24dp, R.string.payment_method_phone),
     TYPE_BANKCARD(R.drawable.ic_payment_white_24dp, R.string.payment_method_bankcard),
-    TYPE_VK_TRANSFER(R.drawable.ic_vk_black, R.string.payment_method_vk_transfer),
-    TYPE_PAYPAL(R.drawable.ic_paypal_black, R.string.payment_method_paypal),
+    //TYPE_VK_TRANSFER(R.drawable.ic_vk_black, R.string.payment_method_vk_transfer),
+    //TYPE_PAYPAL(R.drawable.ic_paypal_black, R.string.payment_method_paypal),
     TYPE_BANK_PAYMENT(R.drawable.ic_account_balance_wallet_black_24dp, R.string.payment_method_bank_payment),
     TYPE_PRIVATE_MESSAGE(R.drawable.ic_forum_black_24dp, R.string.payment_method_private_message);
 
