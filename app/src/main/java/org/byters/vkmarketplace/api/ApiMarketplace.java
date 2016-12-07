82bd6cb9451b30c26dd2751dee095cceded4e645
alexandr motorin <blackdizel@gmail.com>
2016-12-07 13:58:09 +0300

    fix payment method

--- app/src/main/java/org/byters/vkmarketplace/api/ApiMarketplace.java
+++ app/src/main/java/org/byters/vkmarketplace/api/ApiMarketplace.java
@@ -4,11 +4,13 @@ import org.byters.vkmarketplace.model.ResponsePayment;
 
 import retrofit2.Call;
 import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.POST;
 
 public interface ApiMarketplace {
-    String BASE_URL = "192.168.0.95:8000/";
+    String BASE_URL = "http://192.168.0.95:8000/";
 
+    @FormUrlEncoded
     @POST("payment/")
     Call<ResponsePayment> requestPayment(
             @Field("token") String token,
