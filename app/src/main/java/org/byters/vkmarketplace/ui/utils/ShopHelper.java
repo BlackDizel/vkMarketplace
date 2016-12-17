f35234bceaa2e5e3284690e4e858f4fad0daa386
blackdizel <blackdizel@gmail.com>
2016-12-17 22:28:14 +0300

    add shop list item type settings

--- app/src/main/java/org/byters/vkmarketplace/ui/utils/ShopHelper.java
+++ app/src/main/java/org/byters/vkmarketplace/ui/utils/ShopHelper.java
@@ -2,14 +2,17 @@ package org.byters.vkmarketplace.ui.utils;
 
 import android.support.annotation.LayoutRes;
 
+import org.byters.vkmarketplace.BuildConfig;
 import org.byters.vkmarketplace.R;
 
 public class ShopHelper {
 
     @LayoutRes
     public static int getItemLayout() {
-        //return R.layout.view_list_items_overlay;
-        return R.layout.view_list_items_vertical;
+        if (BuildConfig.shopItemType == BuildConfig.shopItemTypeVertical)
+            return R.layout.view_list_items_vertical;
+        else
+            return R.layout.view_list_items_overlay;
     }
 
 }
