aea058b1fe01e51523b028b87b0053afd0e34b33
blackdizel <blackdizel@gmail.com>
2017-03-01 17:14:46 +0300

    add notification types titles

--- app/src/main/java/org/byters/vkmarketplace/model/NotificationTypesEnum.java
+++ app/src/main/java/org/byters/vkmarketplace/model/NotificationTypesEnum.java
@@ -1,18 +1,25 @@
 package org.byters.vkmarketplace.model;
 
+import android.support.annotation.StringRes;
+
+import org.byters.vkmarketplace.R;
+
 import java.util.ArrayList;
 
 public enum NotificationTypesEnum {
-    PROMO("promo"),
-    SALES("sales"),
-    OFFLINE_MARKET("offline_market"),
-    CONTEST("contest"),
-    NOTIFICATION("notification");
+    PROMO("promo", R.string.notification_type_promo),
+    SALES("sales", R.string.notification_type_sales),
+    OFFLINE_MARKET("offline_market", R.string.notification_type_market),
+    CONTEST("contest", R.string.notification_type_contest),
+    NOTIFICATION("notification", R.string.notification_type_service);
 
     private final String type;
+    @StringRes
+    private final int titleRes;
 
-    NotificationTypesEnum(String type) {
+    NotificationTypesEnum(String type, int titleRes) {
         this.type = type;
+        this.titleRes = titleRes;
     }
 
     public static ArrayList<String> getList() {
@@ -24,5 +31,10 @@ public enum NotificationTypesEnum {
 
     public String getType() {
         return type;
+    }
+
+    @StringRes
+    public int getTitleRes() {
+        return titleRes;
     }
 }
