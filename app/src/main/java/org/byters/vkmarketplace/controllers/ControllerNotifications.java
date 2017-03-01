ace334999e58ca5897d5e078bac18bf1b8a513cc
blackdizel <blackdizel@gmail.com>
2017-03-01 17:14:32 +0300

    implement notifications controller methods

--- app/src/main/java/org/byters/vkmarketplace/controllers/ControllerNotifications.java
+++ app/src/main/java/org/byters/vkmarketplace/controllers/ControllerNotifications.java
@@ -2,6 +2,7 @@ package org.byters.vkmarketplace.controllers;
 
 import android.content.Context;
 import android.support.annotation.Nullable;
+import android.support.annotation.StringRes;
 
 import com.google.firebase.messaging.FirebaseMessaging;
 import com.google.firebase.messaging.RemoteMessage;
@@ -14,6 +15,7 @@ import java.util.ArrayList;
 public class ControllerNotifications {
     public static final String TOPICS_TYPE_ALL = "ALL";
     private static ControllerNotifications instance;
+    private ArrayList<String> data;
 
     private ControllerNotifications() {
     }
@@ -39,7 +41,38 @@ public class ControllerNotifications {
         FirebaseMessaging.getInstance().subscribeToTopic(TOPICS_TYPE_ALL);
     }
 
-    public void initNotificationTypes() {
+    void initNotificationTypes() {
         ControllerStorage.getInstance().setNotificationTypesEnabled(NotificationTypesEnum.getList());
+    }
+
+    public int getNotificationTypesNum() {
+        return NotificationTypesEnum.values().length;
+    }
+
+    public boolean isNotificationTypeEnabled(int position) {
+        updateData();
+        return data != null && data.contains(NotificationTypesEnum.values()[position].getType());
+    }
+
+    private void updateData() {
+        if (data == null) data = ControllerStorage.getInstance().getNotificationTypesEnabled();
+    }
+
+    public void setNotificationTypeEnabled(int position, boolean isChecked) {
+        updateData();
+        if (!isChecked) {
+            if (data == null) return;
+            data.remove(NotificationTypesEnum.values()[position].getType());
+        } else {
+            if (data == null) data = new ArrayList<>();
+            if (data.contains(NotificationTypesEnum.values()[position].getType())) return;
+            data.add(NotificationTypesEnum.values()[position].getType());
+        }
+        ControllerStorage.getInstance().setNotificationTypesEnabled(data);
+    }
+
+    @StringRes
+    public int getNotificationTypeTitleResource(int position) {
+        return NotificationTypesEnum.values()[position].getTitleRes();
     }
 }
