1f0582c170e29ec61eb05e0e073e0d086bdeff5a
blackdizel <blackdizel@gmail.com>
2017-03-01 14:09:30 +0300

    add first launch pref, add notifications cache

--- app/src/main/java/org/byters/vkmarketplace/controllers/ControllerStorage.java
+++ app/src/main/java/org/byters/vkmarketplace/controllers/ControllerStorage.java
@@ -2,6 +2,8 @@ package org.byters.vkmarketplace.controllers;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.SharedPreferences;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.util.Log;
 
@@ -11,18 +13,23 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.util.ArrayList;
 
 public class ControllerStorage {
 
     public static final String ITEMS_INFO = "cache_items";
     public static final String TOKEN = "token_cache";
-    public static final String CART_CACHE = "cart_cache";
-    public static final String FAVORITES_CACHE = "favorites_cache";
-    public static final String NEWS_CACHE = "cache_news";
-    public static final String USERINFO_CACHE = "cache_userinfo";
-    public static final String ALBUMS_CACHE = "cache_albums";
-    public static final String ORDERS_HISTORY_CACHE = "orders_cache";
+    static final String CART_CACHE = "cart_cache";
+    static final String FAVORITES_CACHE = "favorites_cache";
+    static final String NEWS_CACHE = "cache_news";
+    static final String USERINFO_CACHE = "cache_userinfo";
+    static final String ALBUMS_CACHE = "cache_albums";
+    static final String ORDERS_HISTORY_CACHE = "orders_cache";
+    private static final String NOTIFICATION_TYPES_CACHE = "cache_notification_types";
+    private static final String PREF_STORAGE = "pref_storage";
+    private static final String PREF_FIRST_LAUNCH = "first_launch";
     private static final String TAG = "controllerStorage";
+
     private static ControllerStorage instance;
     private Context context;
 
@@ -60,8 +67,12 @@ public class ControllerStorage {
         }
     }
 
-    @Nullable
     public synchronized Object readObjectFromFile(String filename) {
+        return readObjectFromFile(context, filename);
+    }
+
+    @Nullable
+    private synchronized Object readObjectFromFile(Context context, String filename) {
         if (context == null) return null;
 
         ObjectInputStream objectIn = null;
@@ -106,5 +117,26 @@ public class ControllerStorage {
         this.context = context;
     }
 
+    ArrayList<String> getNotificationTypesEnabled(Context context) {
+        return (ArrayList<String>) readObjectFromFile(context, NOTIFICATION_TYPES_CACHE);
+    }
 
+    @NonNull
+    private SharedPreferences getPreferences() {
+        if (context == null) throw new IllegalStateException();
+        return context.getSharedPreferences(PREF_STORAGE, Context.MODE_PRIVATE);
+    }
+
+    boolean isFirstLaunch() {
+        boolean isFirst = getPreferences().getBoolean(PREF_FIRST_LAUNCH, true);
+        getPreferences()
+                .edit()
+                .putBoolean(PREF_FIRST_LAUNCH, false)
+                .apply();
+        return isFirst;
+    }
+
+    void setNotificationTypesEnabled(ArrayList<String> list) {
+        writeObjectToFile(list, NOTIFICATION_TYPES_CACHE);
+    }
 }
